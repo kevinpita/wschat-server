b445da5894f1aae1bb510a3db47e88fe0e8dcff9
kevinpita <69063372+kevinpita@users.noreply.github.com>
2023-08-27 22:57:05 +0200

    feat: Create base ws server

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+type Message struct {
+	Data int `json:"data"`
+}
+
+func writeData(ws *websocket.Conn, quit <-chan struct{}) {
+	data := Message{
+		Data: 0,
+	}
+	for {
+		select {
+		case <-quit:
+			log.Println("received stop signal, stopping write")
+			return
+		default:
+			err := ws.WriteJSON(data)
+			if err != nil {
+				log.Println("Error writing message:", err)
+				break
+			}
+			data.Data++
+		}
+	}
+}
+
+func readMessage(ws *websocket.Conn, quit chan struct{}) {
+	for {
+		select {
+		case <-quit:
+			log.Println("received stop signal, stopping read")
+			return
+		default:
+			msgType, _, err := ws.NextReader()
+			if err != nil {
+				log.Println("Error reading next message:", err)
+				close(quit)
+				break
+			}
+
+			if msgType == websocket.CloseMessage {
+				log.Println("received close message, terminating connection")
+				close(quit)
+				break
+			}
+		}
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	upgrader := websocket.Upgrader{}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+	quit := make(chan struct{})
+	go writeData(conn, quit)
+	go readMessage(conn, quit)
+
+	<-quit
+	log.Println("closing connection:", r.RemoteAddr)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	server := &http.Server{
+		Addr:              ":8080",
+		ReadHeaderTimeout: time.Second,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
