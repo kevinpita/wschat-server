dccb74cc41e0d5836fedb85c82f867dfc6cf81b9
kevinpita <69063372+kevinpita@users.noreply.github.com>
2023-08-27 23:36:15 +0200

    fix: Change go version, avoid naked returns and fatals

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module wsserver
 
-go 1.21.0
+go 1.19.0
 
 require github.com/gorilla/websocket v1.5.0 // indirect
--- main.go
+++ main.go
@@ -13,43 +13,49 @@ type Message struct {
 }
 
 func writeData(ws *websocket.Conn, quit <-chan struct{}) {
-	data := Message{
-		Data: 0,
-	}
+	data := Message{}
+loopRead:
 	for {
 		select {
 		case <-quit:
 			log.Println("received stop signal, stopping write")
-			return
+			break loopRead
 		default:
 			err := ws.WriteJSON(data)
 			if err != nil {
 				log.Println("error writing message:", err)
-				break
+				break loopRead
 			}
 			data.Data++
+			time.Sleep(time.Millisecond)
 		}
 	}
 }
 
 func readMessage(ws *websocket.Conn, quit chan struct{}) {
+loopRead:
 	for {
 		select {
 		case <-quit:
 			log.Println("received stop signal, stopping read")
-			return
+			break loopRead
 		default:
 			msgType, _, err := ws.NextReader()
 			if err != nil {
+				if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+					log.Println("received close message, terminating connection")
+					close(quit)
+					break loopRead
+				}
 				log.Println("error reading next message:", err)
 				close(quit)
-				break
+				break loopRead
 			}
 
 			if msgType == websocket.CloseMessage {
 				log.Println("received close message, terminating connection")
 				close(quit)
-				break
+				break loopRead
 			}
 		}
 	}
@@ -72,14 +78,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	const (
+		readSeconds  = 5
+		writeSeconds = 10
+		idleSeconds  = 15
+	)
 	http.HandleFunc("/", handler)
-	server := &http.Server{
-		Addr:              ":8080",
-		ReadHeaderTimeout: time.Second,
+	srv := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  readSeconds * time.Second,
+		WriteTimeout: writeSeconds * time.Second,
+		IdleTimeout:  idleSeconds * time.Second,
 	}
 
-	err := server.ListenAndServe()
+	err := srv.ListenAndServe()
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 }
