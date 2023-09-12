34a9fe7fe08d92da241abf645a1a9652b2a093a2
kevinpita <69063372+kevinpita@users.noreply.github.com>
2023-09-12 13:13:35 +0200

    refactor: Remove extracted methods from main

--- main.go
+++ main.go
@@ -5,61 +5,13 @@ import (
 	"net/http"
 	"time"
 
+	"wsserver/ws"
+
 	"github.com/gorilla/websocket"
 )
 
-type Message struct {
-	Data int `json:"data"`
-}
-
 type server struct {
 	upgrader websocket.Upgrader
-}
-
-func writeData(ws *websocket.Conn, quit <-chan struct{}) {
-	msg := Message{}
-
-	const tickerTime = 30 * time.Millisecond
-	ticker := time.NewTicker(tickerTime)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-quit:
-			log.Println("received stop signal, stopping write")
-			return
-		case <-ticker.C:
-			err := ws.WriteJSON(msg)
-			if err != nil {
-				log.Println("error writing message:", err)
-				return
-			}
-			msg.Data++
-		}
-	}
-}
-
-func readMessage(ws *websocket.Conn, quit chan<- struct{}) {
-	for {
-		msgType, msgData, err := ws.ReadMessage()
-		if err != nil {
-			if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
-				log.Println("connection has been closed by the client")
-			} else {
-				log.Println("error reading next message:", err)
-			}
-			close(quit)
-			return
-		}
-
-		if msgType == websocket.CloseMessage {
-			log.Println("received close message, terminating connection")
-			close(quit)
-			return
-		}
-
-		log.Println("received data:", msgData)
-	}
 }
 
 func (s *server) handler(w http.ResponseWriter, r *http.Request) {
@@ -70,8 +22,8 @@ func (s *server) handler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 	quit := make(chan struct{})
-	go writeData(conn, quit)
-	go readMessage(conn, quit)
+	go ws.WriteData(conn, quit)
+	go ws.ReadMessage(conn, quit)
 
 	log.Println("connection alive:", r.RemoteAddr)
 	<-quit
